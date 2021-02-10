a60049956930b2000b69a3ae17118c15ccf04a2b
Christian Schneider-Davis <75326583+Christian-Schneider-Davis@users.noreply.github.com>
2021-02-10 02:27:32 -0600

    1 down 9 more to go

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const busImage = document.querySelector(".intro")
+
+busImage.addEventListener("mouseenter", () => {
+    busImage.style.transform = "scale(1.2)";
+    busImage.style.transition = "all 0.3s" })
