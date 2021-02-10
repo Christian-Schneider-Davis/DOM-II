6b9ecee2efc1fda5814fd13040a8242491114207
Christian Schneider-Davis <75326583+Christian-Schneider-Davis@users.noreply.github.com>
2021-02-10 02:32:58 -0600

    1 down 9 more to go

--- js/index.js
+++ js/index.js
@@ -4,3 +4,7 @@ const busImage = document.querySelector(".intro")
 busImage.addEventListener("mouseenter", () => {
     busImage.style.transform = "scale(1.2)";
     busImage.style.transition = "all 0.3s" })
+
+    busImage.addEventListener("click", () => {
+        busImage.style.transform = "scale(1)";
+        })
